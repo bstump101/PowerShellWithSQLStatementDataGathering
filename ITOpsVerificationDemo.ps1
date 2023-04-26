0d992fd87554b4fe660c9febc6a16c95bcd55fae
Bruce Stump <brucestump@gmail.com>
2023-04-26 07:48:40 -0400

    Add files via upload

---
+++ ITOpsVerificationDemo.ps1
@@ -0,0 +1,23 @@
+﻿# 1. ITOpsVerification Module
+
+# .Silent cls
+
+# Get available repositories.
+
+Get-PSRepository
+
+# Install ITOpsVerification module from internal PowerShell Repository.
+
+Install-Module ITOpsVerification -Repository PowerShellRepository
+
+# Import-Module ITOpsVerification into current PowerShell Session.
+
+Import-Module -Name ITOpsVerification
+
+# Get Cmdlets in ITOpsVerification.
+
+Get-Command -Module ITOpsVerification | ?{$_.Name -like "Invoke-Lynx*"}
+
+# ------------------------------
+# End of ITOpsVerification Demo.
+# ------------------------------
